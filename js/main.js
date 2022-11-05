2bb34f84b52d3ba8c40814c5bf6228e8d4c14e38
Mario Duarte <marioduartee@hotmail.com>
2022-11-05 16:33:02 -0300

    last commit

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
-function createGame(player1, hour, player2) {
+function createGame(player1, hour, player2, name1, name2) {
     return `
 <li>
-    <img src="./assets/icon-${player1}.svg" alt="logo brasil">
+    <img src="./assets/${player1}.svg" alt="logo brasil">    
     <strong>${hour}</strong>
-    <img src="./assets/icon-${player2}.svg" alt="logo servia">
+    <img src="./assets/${player2}.svg" alt="logo servia">
+    
 </li>
-
+<section><p>${name1}</p>
+<p>${name2}</p>
+</section>
     `
 }
 
@@ -34,20 +37,25 @@ document.querySelector("#app").innerHTML = `
 </header>
 
 <main id="cards">
+${createCard("20/11", "domingo",
+    createGame("qatar", "13:00", "ecuador", "Qatar", "Equador")
+)}
+${createCard("21/11", "terça",
+    createGame("england", "10:00", "iran", "Inglaterra", "Irã") +
+    createGame("senegal", "13:00", "netherlands", "Senegal", "Holanda") +
+    createGame("united-states", "16:00", "wales", "Usa", "Gales")
+)}
+${createCard("22/11", "Quarta",
+    createGame("argentina", "07:00", "saudi arabia", "Argentina", "Arabia Saudita") +
+    createGame("denmark", "10:00", "tunisia", "Hungria", "Argentina") +
+    createGame("mexico", "13:00", "japan", "Mexico", "Polônia") +
+    createGame("france", "16:00", "australia", "Mexico", "Polônia")
+)}
 ${createCard("24/11", "quinta",
-    createGame("brazil", "16:00", "camaroes") +
-    createGame("hungary", "13:00", "argentina") +
-    createGame("colombia", "20:00", "japan")
-)}
-${createCard("28/11", "Segunda",
-    createGame("columbia", "13:00", "camaroes") +
-    createGame("india", "13:00", "armenia") +
-    createGame("comoros", "13:00", "jodan")
-)}
-${createCard("28/11", "Segunda",
-    createGame("columbia", "13:00", "camaroes") +
-    createGame("india", "13:00", "armenia") +
-    createGame("comoros", "13:00", "jodan")
+    createGame("morocco", "07:00", "croatia", "Marrocos", "Croácia") +
+    createGame("germany", "10:00", "japan", "Alemanha", "Japão") +
+    createGame("spain", "13:00", "costa rica", "Espanha", "costa Rica") +
+    createGame("belgium", "16:00", "canada", "Bélgica", "Canadá")
 )}
 
 </main >
